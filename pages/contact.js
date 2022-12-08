088ff7b084a1d084f78836998d19ee17bf39cad0
Ethan <edenvir99@gmail.com>
2022-12-08 19:06:53 +0900

    add: email function

--- pages/contact.js
+++ pages/contact.js
@@ -1,45 +1,51 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function ContactPage() {
-  const [ fullname, setFullname ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ subject, setSubject ] = useState('');
-  const [ message, setMessage ] = useState('');
+  const form = useRef();
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    console.log(form);
+
+    try {
+      await emailjs.sendForm('service_qz6zxh3', 'template_s6ll9hl', form.current, '1PVVpqMraXzVXJuXv');
+    } catch (error) {
+      alert('Failed to send message');
+    }
+  };
 
   return (
     <div className="pt-0 sm:pt-16">
-      <form name="contact" method="POST" data-netlify="true" className="rounded-lg shadow-lg flex flex-col w-12/12 px-8 py-8 bg-gray-900 sm:w-9/12 sm:m-auto">
+      <form name="contact" ref={ form } onSubmit={ sendEmail } className="rounded-lg shadow-lg flex flex-col w-12/12 px-8 py-8 bg-gray-900 sm:w-9/12 sm:m-auto">
         <h1 className="text-2xl font-bold text-gray-50">Send a message</h1>
 
         <label for="fullname" className="text-gray-50 font-light mt-8">Full Name<span className="text-red-500">*</span></label>
         <input
           type="text"
-          name="fullname"
+          name="from_name"
           className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-white"
-          onChange={ (e) => setFullname(e.target.value) }
         />
 
         <label for="email" className="text-gray-50 font-light mt-4">Email Address<span className="text-red-500">*</span></label>
         <input
           type="email"
-          name="email"
+          name="from_email"
           className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-white"
-          onChange={ (e) => setEmail(e.target.value) }
         />
 
-        <label for="subject" class="text-gray-50 font-light mt-4">Subject<span className="text-red-500">*</span></label>
+        <label for="subject" className="text-gray-50 font-light mt-4">Subject<span className="text-red-500">*</span></label>
         <input
           type="text"
-          name="subject"
+          name="message"
           className="bg-transparent border-b py-2 pl-4 focus:outline-node focus:rounded-md focus:ring-1 ring-green-500 font-light text-white"
-          onChange={ (e) => setSubject(e.target.value) }
         />
 
         <label for="message" className="text-gray-50 font-light mt-4">Message<span className="text-red-500">*</span></label>
         <textarea
           name="message"
           className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-white"
-          onChange={ (e) => setMessage(e.target.value) }
         >
         </textarea> 
 
